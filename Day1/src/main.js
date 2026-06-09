8d29fcb065d7fe4307323a8ef10eb48bc41ee5de
Madhurchhetri <tandonnakul987@gmail.com>
2026-06-09 22:30:02 +0530

    gsap learn method deeply to, from , fromto and also learn how to run function gsap

---
+++ Day1/src/main.js
@@ -0,0 +1,38 @@
+import gsap from "gsap";
+import './style.css';
+
+const box = document.querySelector('.box');
+
+// gsap.to(box,{
+//   x: 400,
+//   duration: 2,
+//   delay: 0.6,
+// })
+
+// gsap.from(box,{
+//   x: 400,
+//   duration: 2,
+//   delay: 0.6,
+// })
+
+// gsap.fromTo(box,{
+//   x: 0,
+// },{
+//   x: 400,
+//   delay: 0.6,
+//   duration: 1,
+//   y: 200
+// },
+// );
+
+// const obj ={
+//   a: 0,
+// }
+
+// gsap.to(obj,{
+//   a: 100,
+
+//   onUpdate:()=>{
+//     console.log(obj.a);
+//   }
+// })
